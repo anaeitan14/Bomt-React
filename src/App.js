785977672d17218c7a858f6f7a5b75596e76c66a
eitan <eitanlich@yahoo.com>
2023-03-23 15:20:24 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,56 @@
+import "./Login.css";
+import {useState, useEffect} from 'react';
+import emailLogo from "../resources/icons/envelope.png";
+
+const Login = () => {
+
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+
+    console.log("This is the username" + email);
+    console.log("This is the password" + password);
+    
+
+    const fetchData = () => {
+        return fetch("http://172.20.26.41:5000/api")
+            .then((response)=>response.json())
+            .then((data)=>console.log(JSON.stringify(data)));
+    }
+
+    useEffect(() => {
+        fetchData();
+    },[])
+
+    return(
+        <div className="bubble">
+            <div className="custom-shape-divider-bottom-1678793596">
+                <svg id="email-svg" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
+                </svg>
+            </div>
+            <form className="login-form">
+                <h1>Welcome to BOMT</h1>
+                <h2>Login</h2>
+                <input 
+                    type="text"
+                    onChange={(e)=>setEmail(e.target.value)} 
+                    className="email-input"
+                    placeholder="Email"/>
+                <input 
+                    type="password"
+                    onChange={(e)=>setPassword(e.target.value)} 
+                    className="password-input"
+                    placeholder="Password"/>
+                <div className="login-buttons">
+                    <button>Login</button>
+                    <button>Sign up</button>
+                </div>
+                <div className="forgot-password">
+                    <button>Forgot password?</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Login
