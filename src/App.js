9100f9d4e4657b86279cc854cad918498228f829
eitan <eitanlich@yahoo.com>
2023-03-31 13:45:09 +0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
@@ -6,9 +6,12 @@ import Signup from "./components/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="signup" element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
