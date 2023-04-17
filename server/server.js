8e3d07ef3517487970e9d82adfa9f328f5641308
eitan <eitanlich@yahoo.com>
2023-04-17 15:46:41 +0300

    Added gitignore and finished entire backend :)

--- server/server.js
+++ server/server.js
@@ -6,13 +6,12 @@ app.use(cors())
 
 mongoose.connect('mongodb://127.0.0.1:27017/hotel');
 
-
-
-
-
-
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
+
+app.get("/eranAPI", (req, res) => {
+    res.json({"Eran":["Hi","Do","Not","Delete","me",":)"], "Eitan":["No","I","Will","Delete","this",":RAGE:"]})
 })
 
 app.listen(5000, () => {console.log("Server is listening on port 5000")})
