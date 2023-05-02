63ad49e7cd6eb0245f27c2605b838735f831427c
Eran Fichman <eran@caliyatech.com>
2023-05-02 13:22:18 +0300

    finally starting i guess

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
 const express = require('express')
 const cors = require('cors')
+const crypto = require('crypto')
 const mongoose = require('mongoose')
 const app = express()
 app.use(cors())
+mongoose.connect('mongodb://127.0.0.1:27017/Users')
+//.then(() => app.listen (5000).then(() => console.log("connected to the database and listening to the port"))).catch((err) => console.log(err));
+app.post
 
-mongoose.connect('mongodb://127.0.0.1:27017/hotel');
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
-})
+const users = mongoose.model('users', userSchema);
 
-app.get("/eranAPI", (req, res) => {
-    res.json({"Eran":["Hi","Do","Not","Delete","me",":)"], "Eitan":["No","I","Will","Delete","this",":RAGE:"]})
-})
+const email = "test@test.com"
+const password = "123456"
+const hash = crypto.createHash('sha256').update(password).digest('hex')
+console.log(hash)
 
-app.listen(5000, () => {console.log("Server is listening on port 5000")})   
+users.insertMany([{email:email, password:hash}])
+
