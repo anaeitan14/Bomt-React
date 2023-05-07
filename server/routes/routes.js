0073de74d7df57b5077f1785f96e27afeecb77f0
uwus <eran@caliyatech.com>
2023-05-07 15:04:28 +0300

    waiting for front

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const loginController = require('../controllers/loginController')
+const itemController = require('../controllers/itemController')
 
 router.post('/register', loginController.register)
 router.post('/login', loginController.login)
 router.post('/google-register', loginController.googleRegister)
+router.post('/addItem', itemController.addItem)
 
 module.exports = router;
