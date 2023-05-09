945f7c1572b4cdafa17dc7c4a20cb99555c65cd5
eitan <eitanlich2000@gmail.com>
2023-05-09 18:06:57 +0300

    Basic search working

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "../components/Header";
 import Table from "../components/Table";
 import Sidebar from "../components/Sidebar";
@@ -7,26 +9,43 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
 export const Home = () => {
+  const [searchQuery, setSetQuery] = useState("");
+  const [item, setItem] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const query = {
+      pid: searchQuery,
+    };
+
+    const URL = "http://localhost:5000/api/searchItem";
+
+    const response = await axios.post(URL, query);
+    const data = response.data;
+    setItem(data);
+  };
+
   return (
     <div className="container-fluid p-0">
       <Header />
       <Sidebar />
-      <div className="d-flex justify-content-around m-2">
-        <InputGroup className="mb-3 w-25">
-          <InputGroup.Text id="basic-addon1">&#x1F50E;&#xFE0E;</InputGroup.Text>
-          <Form.Control
-            onChange={() => {
-              alert("ok");
-            }}
-            placeholder="Item"
-            aria-label="Item"
-            aria-describedby="Item"
-          />
-        </InputGroup>
-        <AddBtn />
-        <RemoveBtn />
-      </div>
-      <Table />
+      <form onSubmit={handleSubmit}>
+        <div className="d-flex justify-content-around m-2">
+          <InputGroup className="mb-3 w-25">
+            <Form.Control
+              onChange={(e) => {
+                setSetQuery(e.target.value);
+              }}
+              placeholder="Item"
+            />
+            <button id="basic-addon1">&#x1F50E;&#xFE0E;</button>
+          </InputGroup>
+          <AddBtn />
+          <RemoveBtn />
+        </div>
+      </form>
+      <Table data={item} />
     </div>
   );
 };
