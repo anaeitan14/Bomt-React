1117b99e43dcd03915d12ee21408c9d639e39e24
uwus <sbeit307@gmail.com>
2023-06-06 13:35:06 +0300

    :)

--- server/middlewares/middlewares.js
+++ server/middlewares/middlewares.js
@@ -2,7 +2,6 @@ const User = require("../models/userSchema");
 
 exports.sessionCheck = (req, res, next) => {
   const { email } = req.body.email;
-  const uid = User.findOne({ email });
-  console.log(uid);
+  const user = User.findOne({ email });
   next();
 };
