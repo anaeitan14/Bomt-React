b37e880cbf7efffdd4084ba110ba80c1e54dce42
eitan <eitanlich2000@gmail.com>
2023-08-25 11:01:17 +0300

    g

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,8 +12,9 @@ export const Home = () => {
   });
 
   const [searchQuery, setQuery] = useState("");
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   const [email, setEmail] = useState("");
+  const [sons, setSons] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -29,6 +30,22 @@ export const Home = () => {
     try {
       response = await instance.post(URL, query);
       setData(response.data);
+      setSons(response.data.Sons);
+      if (sons.length > 0) {
+        for (let i = 0; i < sons.length; i++) {
+          try {
+            query.pid = sons[i];
+            console.log(query);
+            let childResponse = await instance.post(URL, query);
+            console.log(childResponse);
+            if (childResponse) {
+              setData(data.concat(childResponse.data));
+            }
+          } catch (e) {
+            console.log("no childs");
+          }
+        }
+      }
     } catch (err) {
       if (err.response.status === 400) {
         alert(err.response.data.message);
@@ -54,7 +71,7 @@ export const Home = () => {
           </div>
           <AddBtn />
           <RemoveBtn />
-          <ChildBtn data={searchQuery}/>
+          <ChildBtn data={searchQuery} />
         </form>
       </div>
       <Table data={data} />
