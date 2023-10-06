4614595c87bb73b26f9dafbe8ad9cf87b40548a7
Eitan <eitanlich2000@gmail.com>
2023-10-06 21:20:41 +0300

    fwe

--- client/src/pages/Queries.jsx
+++ client/src/pages/Queries.jsx
@@ -2,17 +2,28 @@ import Navbar from "../components/Navbar";
 
 const Queries = () => {
   const getQueries = () => {
+    let data = localStorage.getItem("Queries");
+    if (!data) {
+      return <div className="empty-logs">No queries available.</div>;
+    }
+    data = data.replace(",", " ");
+    let res = data.split(" ");
 
-    const data = localStorage.getItem("Queries");
-
-    data.map(() => {
-
+    return res.map((query, i) => {
+      if (query !== "") {
+        return (
+          <li key={i} className="log-item">
+            {"Query " + i + " = " + query}
+          </li>
+        );
+      }
     });
   };
 
   return (
     <>
       <Navbar />
+      <ul className="logs-list">{getQueries()}</ul>
     </>
   );
 };
